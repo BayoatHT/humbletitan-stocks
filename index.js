f4b4b8c33790b5ea0218af580a4394a453f2783b
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-01-26 16:07:24 +0500

    make it better

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const cors = require('cors')
 const app = express()
 app.use(express.static(path.join(__dirname, './')));
 
-app.use(cors({
-  origin: 'https://designinguru.com'
-}));
+app.use(cors());
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 
 AWS.config.update({ region: 'us-east-1' });
  
