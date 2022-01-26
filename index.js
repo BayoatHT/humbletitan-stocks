77944a2ea337e7bdaba00c86b4dda94b021065eb
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-01-26 13:30:43 +0500

    make it better

--- index.js
+++ index.js
@@ -8,17 +8,53 @@ app.use(express.static(path.join(__dirname, './')));
 
 
 AWS.config.update({ region: 'us-east-1' });
- 
+const dynamoClient = new AWS.DynamoDB.DocumentClient();
+app.get('/',(req,res)=>{
+  res.send("Guddi MC")
+})
 
- app.get('/',(req,res)=>{
-   res.send("GUDDI CHUTIYA")
- })
 
- app.get('/mudassir',(req,res)=>{
-   res.json({
-     "Guddi":"Bhrwa"
-   })
- })
+const getDataById = async(id, TABLE_NAME)=>{
+  const params ={
+      TableName:TABLE_NAME,
+      Key:{
+          Symbol:  id
+      }
+  }
+  const data = await dynamoClient.get(params).promise()
+  // console.log(data)
+  return data
+}
+
+
+app.get('/companydetails/:symbol',async(req,res)=>{
+  const PROFILE_TABLENAME = "CompanyProfile"
+  const SHARES_TABLENAME = "SharesFloat"
+  const FINANCIALRATIO_TABLENAME = "FinancialRatiosTTM"
+  const KEYMETRICS_TABLENAME = "KeyMetricsTTM"
+  const RATING_TABLENAME = "Ratings"
+  const REALTIMEQUOTES_TABLENAME = "RealTimeQuotes"
+  const FINANCIALGROWTH_TABLENAME = "FinancialGrowthANN"
+  const PEERS_TABLENAME = "Peers"
+  const id = req.params.symbol
+
+  try {
+    const companyProfile = await getDataById(id, PROFILE_TABLENAME)
+    const companyShares = await getDataById(id, SHARES_TABLENAME)
+    const comapnyFinancialRatio = await getDataById(id, FINANCIALRATIO_TABLENAME)
+    const comapnyKeymetrics = await getDataById(id, KEYMETRICS_TABLENAME)
+    const comapnyRating = await getDataById(id, RATING_TABLENAME)
+    const comapnyRealTimeQuote = await getDataById(id, REALTIMEQUOTES_TABLENAME)
+    const comapnyFinancialGrowth = await getDataById(id, FINANCIALGROWTH_TABLENAME) 
+
+    res.json({companyProfile, companyShares, comapnyFinancialRatio, comapnyKeymetrics, comapnyRating, comapnyRealTimeQuote, comapnyFinancialGrowth})
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ err: 'Something went wrong' })
+    return;
+  }
+})
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`listening on port`)
